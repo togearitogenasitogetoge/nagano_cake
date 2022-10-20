12942e5a8709ce60e7afd3681866188f501c590d
YukikoNiwa <112695050+iamyukiko@users.noreply.github.com>
2022-10-20 04:26:51 +0000

    テスト

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,17 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :deliveries
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    self.family_name + " " + self.first_name
+  end
+
+  def kana_full_name
+    self.kana_family_name + " " + self.kana_first_name
+  end
+
 end
