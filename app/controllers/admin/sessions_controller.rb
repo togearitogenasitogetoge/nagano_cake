c568ea922ef4a9984f89d5971877c37fbaa43416
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-21 13:32:09 +0000

    f.submit解決？

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -30,7 +30,7 @@ class Admin::SessionsController < Devise::SessionsController
     admins_path
   end
 
-  def afte_sign_out_path_for(resource)
+  def after_sign_out_path_for(resource)
     new_admin_session_path
   end
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,13 +1,20 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :cart_products , dependent: :destroy
   has_many :orders , dependent: :destroy
   has_many :deliveries , dependent: :destroy
+  validates :family_name , presence: true
+  validates :first_name , presence: true
+  validates :kana_family_name , presence: true
+  validates :kana_first_name , presence: true
+  validates :postal_code , presence: true
+  validates :address , presence: true
+  validates :phone_number , presence: true
 
   def full_name
     self.family_name + " " + self.first_name
