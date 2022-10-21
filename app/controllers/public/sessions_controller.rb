e4d7d35b1f03304ad796fd6ca203aa1a01c9381e
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-21 16:04:55 +0000

    customer機能完成(仮)

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  #before_action :reject_inactive_customer,only: [:create]
+  before_action :reject_inactive_customer,only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -9,10 +9,10 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   #POST /resource/sign_in
+   #def create
+   # super
+   #end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -26,16 +26,16 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  #def reject_inactive_customer
-    #@customer = Customer.find_by(email: params[:customer][:email])
-    #if @customer
-      #if @customer.vaild_password?(params[:customer][:password]) && !@customer.customer_status
-        #redirect_to new_customer_session_path
-      #end
-    #end
-  #end
-
   protected
+
+   def reject_inactive_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@customer.customer_status
+        redirect_to new_customer_session_path
+      end
+    end
+   end
 
   def after_sign_in_path_for(resource)
     customers_my_page_path
