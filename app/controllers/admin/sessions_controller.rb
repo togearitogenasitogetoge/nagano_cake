69c68e1fb8ea847765259a38c8a0ade6666fdb3b
YukikoNiwa <112695050+iamyukiko@users.noreply.github.com>
2022-10-22 04:11:07 +0000

    配送先一覧修正　フッターアイコン修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -21,9 +21,9 @@ class Admin::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+   def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+   end
   protected
 
   def after_sign_in_path_for(resource)
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,16 +26,16 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  #def reject_inactive_customer
-    #@customer = Customer.find_by(email: params[:customer][:email])
-    #if @customer
-      #if @customer.vaild_password?(params[:customer][:password]) && !@customer.customer_status
-        #redirect_to new_customer_session_path
-      #end
-    #end
-  #end
-
   protected
+
+  def reject_inactive_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@customer.customer_status
+        redirect_to new_customer_session_path
+      end
+    end
+  end
 
   def after_sign_in_path_for(resource)
     customers_my_page_path
