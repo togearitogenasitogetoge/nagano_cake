afc99ab19846b385893e294301b131091eaa0f72
YukikoNiwa <112695050+iamyukiko@users.noreply.github.com>
2022-10-22 08:16:49 +0000

    カート機能編中　エラー有

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -7,7 +7,7 @@ class Public::ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    #@cart_item = CartItem.new
+    @cart_product = CartProduct.new
     #genre = Genre.all
 
   end
