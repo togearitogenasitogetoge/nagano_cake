29db9c715a162c694f5dcff64fcefadee24f2417
EC2 Default User <ec2-user@ip-172-31-8-173.ap-northeast-1.compute.internal>
2022-10-24 04:08:01 +0000

    保存クリア

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -15,6 +15,7 @@ class Public::CartProductsController < ApplicationController
 
   def index
     @cart_products = current_customer.cart_products
+    @total = @cart_products.inject(0) { |sum, product| sum + product.subtotal }
   end
 
   def update
@@ -35,20 +36,6 @@ class Public::CartProductsController < ApplicationController
     redirect_to cart_products_path
   end
 
-  def create
-    @product = Product.find(cart_product_params[:product_id])
-    @cart_product = CartProduct.new(cart_product_params)
-    @cart_product.customer_id = current_customer.id
-  # if current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id]).present?
-		# 	current_customer.cart_product.find_by(product_id: params[:cart_product][:product_id]).cart_product.quantity += params[:cart_product][:quantity].to_i
-      # @cart_product.save
-		# 	redirect_to cart_products_path
-		# elsif @cart_product.save
-		# 	redirect_to cart_products_path
-		# else
-		  # render "public/products/show"
-		# end
-  end
 
   private
 
