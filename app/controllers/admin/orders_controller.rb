9b4f648f7014058714b166d971051559cb5c0893
EC2 Default User <ec2-user@ip-172-31-1-182.ap-northeast-1.compute.internal>
2022-10-25 08:28:03 +0000

    add status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,20 @@
 class Admin::OrdersController < ApplicationController
 
   def show
-    @customer=Customer.find(params[:id])
-    @order=Order.find(params[:id])
-    @order_product=@order.order_products
+    @order = Order.find(params[:id])
+    @order_products = @order.order_products
+
+
   end
 
   def update
     @order=Order.find(params[:id])
-    if @order.update(order_status_params)
-      @order.order_product.update_all(status:1) if @order.status == 1
+    @order_product = @order.order_products
+    @order.update(order_status_params)
+    if @order.order_status == "入金確認"
+      @order_product.update_all(work_status:"制作待ち")
+      redirect_to request.referer
+    else
       redirect_to request.referer
     end
   end
