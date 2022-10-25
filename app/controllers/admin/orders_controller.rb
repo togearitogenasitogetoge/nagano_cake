b68505b0061f69bb558926f6bbf5a6d07c25321e
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-25 09:26:39 +0000

    add status2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,19 +2,17 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order=Order.find(params[:id])
-    #@customer=@order.customer
     @order_product=@order.order_products
-    #@product=@order_product.products
-    #@total = @order_products.inject(0) { |sum, order_product| sum + (order_product.product.tax_excluded_price * order_product.quantity) }
-    #@total = @total * 1.1
-    #@order.request_amount = @total + 800
+    @total = @order_product.inject(0) { |sum, order_product| sum + (order_product.product.tax_excluded_price * order_product.quantity) }
+    @total = @total * 1.1
+    @order.request_amount = @total + 800
   end
 
   def update
     @order=Order.find(params[:id])
     @order_product=@order.order_products
     @order.update(order_status_params)
-      if @order.order_status == "payment_confirmation"
+      if @order.order_status == "入金確認"
       @order_product.update_all(work_status: "制作待ち")
       redirect_to request.referer
       else
