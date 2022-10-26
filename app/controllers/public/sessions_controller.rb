3acb30d4344f7f3a471307b9887bb1ef0e9e2e94
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-26 04:08:13 +0000

    fix test

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -37,11 +37,6 @@ class Public::SessionsController < Devise::SessionsController
     end
    end
 
-
-  def after_sign_in_path_for(resource)
-    customers_my_page_path
-  end
-
   def after_sign_out_path_for(resource)
     new_customer_session_path
   end
