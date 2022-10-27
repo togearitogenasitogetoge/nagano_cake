a02accbd568b113ca7a3037fb81ec818461c2738
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-27 05:30:02 +0000

    fix footer

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,6 +13,6 @@ class Order < ApplicationRecord
 
 
   enum payment_method: {credit_card:0,transfer:1}
-  enum order_status:{"入金待ち":0, "入金確認": 1, "製作中":2, "発送準備中":3, "発送済み":4}
+  enum order_status:{payment_waiting:0, payment_confirmation: 1, in_production:2, preparing_delivery:3, delivered:4}
 
 end
